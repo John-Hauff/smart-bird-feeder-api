0b9331916a1cd6fca3a2b9ecfc9cb62d40a6b4da
John Hauff <jthauff@gmail.com>
2022-05-01 10:59:10 -0400

    Modified user.js
    Changed quotations from double to single quotes

--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,37 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
-const userController = require("../controllers/user");
-const birdMemoriesController = require("../controllers/BirdMemories");
+const userController = require('../controllers/user');
+const birdMemoriesController = require('../controllers/BirdMemories');
 
-const multer = require("multer");
+const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: function (req, res, cb) {
     // Use callback function to store image in specified local dir
-    cb(null, path.join(__dirname, "../bird_memory_uploads/"));
+    cb(null, path.join(__dirname, '../bird_memory_uploads/'));
   },
 });
 
 const upload = multer({ storage: storage });
 
 // Signup
-router.post("/signup", userController.signup);
+router.post('/signup', userController.signup);
 // Sign in
-router.post("/signin", userController.signin);
+router.post('/signin', userController.signin);
 // Upload bird memories for a user
 router.post(
-  "/post-bird-memory",
-  upload.single("file"),
+  '/post-bird-memory',
+  upload.single('file'),
   birdMemoriesController.postBirdMemory
 );
 // Get the bird memories for a user
-router.get("/get-bird-memory", birdMemoriesController.getBirdMemory);
+router.get('/get-bird-memory', birdMemoriesController.getBirdMemory);
 
 router.post(
-  "/post-bird-memory-notification",
+  '/post-bird-memory-notification',
   birdMemoriesController.postNotification
 );
 
